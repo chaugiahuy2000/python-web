8d2a5d24dba632495605fb3c78a1714e2e2d8def
onetech-dev <dev@onetech.vn>
2022-10-07 17:14:51 +0700

    setting 3

--- EStore/settings.py
+++ EStore/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_8&y0*-^t%4^0e6dr(wci)^iq1hp6var&!2_l#n=nu!e!5s&c3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['shop-gear.herokuapp.com', 'localhost', '127.0.0.1']
 
